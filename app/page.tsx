c110a66a4e66417d10bbf09c3493adb1f9d4cc33
harshal <harshalsomkuwar9@gmail.com>
2026-04-02 20:19:46 +0530

    Fixed up some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import Image from "next/image";
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
 
+  const router = useRouter()
 
+  useEffect(() => {
+
+    router.push("/login")
+
+  } , [])
   
   
   return (
     <div>
-      
-      <h1>
-        Hello World
-      </h1>
 
     </div>
   );
